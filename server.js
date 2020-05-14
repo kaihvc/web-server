86a40c386eaf428472295bdbdb217e2f946e6a78
kaihvc <47830342+kaihvc@users.noreply.github.com>
2020-05-14 00:53:42 -0600

    Initial commit - packages, server, and home.ejs

---
+++ server.js
@@ -0,0 +1,45 @@
+/*
+  This is my NodeJS server!
+  Turns out a semester or two of undealt-with trauma makes it hard to forkus on study
+*/
+
+/*
+  First: necessary components
+
+  Express         - A NodeJS web server framework
+
+*/
+
+//Set port config variable
+const PORT = process.env.PORT || 3000; //Default process port or 3000
+
+//Require express
+const express = require('express'); //Express framework
+
+/*
+Quick note: Require essentially reads/executes a JS file and returns the associated exports object
+*/
+
+//Create web app and set up config
+var app = express();
+app.use(express.static(__dirname + '/')); //Set current directory as root
+app.set('view engine', 'ejs'); //Mostly cause I'm familiar with ejs
+
+/*
+  HTTP requests! Awesome!
+*/
+
+app.get('/', function(req, res){
+
+  res.redirect('/home');
+
+});
+
+app.get('/home', function(req, res){
+
+  res.render('/pages/home');
+
+});
+
+app.listen(PORT);
+console.log(PORT + ' is the magic port');
